63bfcb8f280dbb0b948b1051726369b6f6c94baf
Santi Espinosa <saigesp@gmail.com>
2024-10-02 16:41:01 +0200

    feat: prevent scroll on map

--- js/map.js
+++ js/map.js
@@ -1,5 +1,8 @@
 
-const map = L.map('map').setView([39.8376017, -4.3978819], 5);
+const map = L.map('map', {
+    gestureHandling: true
+}).setView([39.8376017, -4.3978819], 5);
+
 L.tileLayer.provider('CartoDB.Voyager').addTo(map);
 
 const markers = [];
